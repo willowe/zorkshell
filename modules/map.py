a25585f6717702db8b2245d1b3f98bded5cc371f
Will Lowe <will.lowe@gmail.com>
2017-11-08 11:17:54 -0800

    * vastly improved room/motion detection * first implementation of inventory tracking

--- modules/map.py
+++ modules/map.py
@@ -15,20 +15,29 @@ ignore = { "You can't go that way.\r\n>": 1,
 motion_commands = [ "north", "south", "east", "west", "up", "down", "ne", "se", "nw", "sw" ]
 
 # room names are sequences of capitalized words
-# still need to handle "Bottom of Well"
-room_name_regexp = re.compile( "^[A-Z][a-z\-]+\s*(of|to|[A-Z][a-z\-]+\s*)*$" )
+# except for a few like "Bottom of Well" and "Entrance to Hades"
+room_name_regexp = re.compile( "^[A-Z][a-z\-\']+\s*(of\s|to\s|[A-Z][a-z\-\']+\s*)*$" )
+
+# needs to handle: "You can see:  foo"
+inventory_regexp = re.compile( "^There (?:is|are) (.*) here.$" )
 
 class ZorkRoom:
     def __init__( self, name ):
         self.name = name
         self.descr = None
         self.directions = {}
-
+        self.inventory = []
+        
     def add_exit( self, dir, name ):
         self.directions[dir] = name
 
     def update_description( self, text ):
         self.descr = text
+
+    def update_inventory( self, args ):
+        self.inventory = []
+        for a in args:
+            self.inventory.append( a )
 
 class ZorkMap( ZorkModule ):
     
@@ -56,6 +65,7 @@ class ZorkMap( ZorkModule ):
 
         lines = re.split( "\r\n", text )
 
+        # what room are we in?
         if room_name_regexp.match( lines[0] ):
 
             name = lines[0]
@@ -65,20 +75,39 @@ class ZorkMap( ZorkModule ):
                 self.rooms[name] = r
             
             if self.current_room and is_motion:
-                log( "map: adding exit %s to %s: %s" % ( last_action, self.current_room.name, name )) 
+                debug( "map: adding exit %s to %s: %s" % ( last_action, self.current_room.name, name )) 
                 self.current_room.add_exit( last_action, name )
+
 
             self.current_room = self.rooms[name]
 
         else:
             
             if re.match( "^l(ook)?", last_action ) and text.startswith( "You are" ) and self.current_room:
-                log( "map: updated room description for %s" % self.current_room.name )
+                debug( "map: updated room description for %s" % self.current_room.name )
                 self.current_room.update_description( text )
-            else:
+            elif is_motion:
                 self.current_room = None
+
+        inventory = []
+        for l in lines:
+            you_can_see = False
+            if you_can_see:
+                inventory.append( l )
+            else:
+                m = inventory_regexp.match( l )
+                if m != None:
+                    inventory.append( m.group(1) )
+            if l.startswith( "You can see: " ):
+                you_can_see = True
+                inventory.append( l.split( ": " )[1] )
+
+        if self.current_room:
+            if len(inventory) > 0:
+                log( "map: found %d items: %s" % ( len(inventory), ",".join(inventory) ))
+            self.current_room.update_inventory( inventory )
                 
-        # todo:  keep an inventory of what's in each room based on "you can see:"
+
         
     def save_map( self, z, args ):
 
@@ -105,6 +134,16 @@ class ZorkMap( ZorkModule ):
 
         log( "loaded map to %s" % filename )
 
+    def find_item( self, z, args ):
+
+        text = args.pop()
+
+        log( "map: searching for an item matching \"%s\"" % text )
+        
+        for n,r in self.rooms.items():
+            for i in r.inventory:
+                if i.find( text ):
+                    log( "map: %s: %s" % ( n, i ) )
         
     def __init__( self ):
         self.rooms = {}
@@ -112,6 +151,7 @@ class ZorkMap( ZorkModule ):
         register_zorkshell_command( "/map", self.show_map )
         register_zorkshell_command( "/savemap", self.save_map )
         register_zorkshell_command( "/loadmap", self.save_map )
+        register_zorkshell_command( "/finditem", self.find_item )
         log( "map: please make sure \"brief\" descriptions are on." )
         
 
